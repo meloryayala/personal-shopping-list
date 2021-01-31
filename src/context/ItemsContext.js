e814be1da7d87eb916801f37be8dfced605aa9ef
Melory Ayala <melory.ayala@gmail.com>
2021-01-31 14:04:30 +0100

    Creating POST addItem on item context

--- src/context/ItemsContext.js
+++ src/context/ItemsContext.js
@@ -23,6 +23,12 @@ const reducer = (state, action) => {
                 loading: false,
                 error: action.payload
             };
+        case 'ADD_ITEMS_SUCCESS':
+            return {
+                ...state,
+                items: [...state.items, action.payload],
+                loading: false,
+            };
         default:
             return state;
     }
@@ -46,15 +52,48 @@ export const ItemsContextProvider = ({children}) => {
             const result = await data.json();
 
             if (result) {
-            dispatch({ type: 'GET_ITEMS_SUCCESS', payload: result })
+                dispatch({type: 'GET_ITEMS_SUCCESS', payload: result})
             }
         } catch (e) {
-            dispatch({ type: 'GET_ITEMS_ERROR', payload: e.message })
+            dispatch({type: 'GET_ITEMS_ERROR', payload: e.message})
         }
     }, [])
 
+    const addItem = useCallback(async ({listId, title, quantity, price}) => {
+        const itemId = Math.floor(Math.random() * 100)
+
+        try {
+            const data = await fetch('https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/items',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        id: itemId,
+                        listId,
+                        title,
+                        quantity,
+                        price,
+                    }),
+                },
+            );
+
+            const result = await data.json();
+
+            if (result) {
+                dispatch({ type: 'ADD_ITEMS_SUCCESS',
+                    payload: {
+                    id: itemId,
+                        listId,
+                        title,
+                        quantity,
+                        price,
+                    },
+                });
+            }
+        } catch {}
+    }, [])
+
     return (
-        <ItemsContext.Provider value={{...state, fetchItems}}>
+        <ItemsContext.Provider value={{...state, fetchItems, addItem}}>
             {children}
         </ItemsContext.Provider>
     )
