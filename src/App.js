4ab253ca325fcd30f51e3977ab6e00bee55c9818
Melory Ayala <melory.ayala@gmail.com>
2021-02-02 15:28:43 +0100

    Adding Suspense & lazy loading to increase performance

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import styled, {createGlobalStyle} from 'styled-components';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Header from './components/Header/Header';
-import Lists from './pages/Lists';
-import ListDetail from './pages/ListDetail';
-import ListForm from './pages/ListForm';
 import AppContext from "./context/AppContext";
+
+const Lists = lazy(() => import('./pages/Lists'));
+const ListDetail = lazy(() => import('./pages/ListDetail'));
+const ListForm = lazy(() => import('./pages/ListForm'));
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -30,13 +31,15 @@ function App() {
             <AppWrapper>
                 <BrowserRouter>
                     <Header/>
-                    <AppContext>
-                        <Routes>
-                            <Route path='/' element={<Lists/>}/>
-                            <Route path='/list/:listId/new' element={<ListForm/>}/>
-                            <Route path='/list/:listId' element={<ListDetail/>}/>
-                        </Routes>
-                    </AppContext>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <AppContext>
+                            <Routes>
+                                <Route path='/' element={<Lists/>}/>
+                                <Route path='/list/:listId/new' element={<ListForm/>}/>
+                                <Route path='/list/:listId' element={<ListDetail/>}/>
+                            </Routes>
+                        </AppContext>
+                    </Suspense>
                 </BrowserRouter>
             </AppWrapper>
         </>
